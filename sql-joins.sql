b050e0fd37140981bde290b7596e3660a6303818
Seun Dada <seundd91@gmail.com>
2023-10-13 14:47:04 +1100

    Update sql-join file

--- sql-joins.sql
+++ sql-joins.sql
@@ -13,7 +13,15 @@ INNER JOIN students s
 WHERE p.student_id = s.student_id;
 
 SELECT * 
+FROM project p
+LEFT JOIN students s 
+ON s.student_id = p.student_id
+WHERE s.student_id IS NULL;
+
+SELECT * 
 FROM students s
-LEFT JOIN projects p
+RIGHT JOIN projects p
 ON p.student_id = s.student_id
-WHERE p.project_id IS NULL;
+WHERE s.student_id IS NULL;
+
+SELECT * FROM projects;
